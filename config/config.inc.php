ee075bc6861a002976c9d093ff4e02d9ddf6cf54
tallesbarros28 <t.barros99@outlook.com>
2024-07-16 15:42:20 -0300

    Update config.inc.php

--- config/config.inc.php
+++ config/config.inc.php
@@ -24,8 +24,8 @@ $_DVWA[ 'db_port']      = '3306';
 # ReCAPTCHA settings
 #   Used for the 'Insecure CAPTCHA' module
 #   You'll need to generate your own keys at: https://www.google.com/recaptcha/admin
-$_DVWA[ 'recaptcha_public_key' ]  = '';
-$_DVWA[ 'recaptcha_private_key' ] = '';
+$_DVWA[ 'recaptcha_public_key' ]  = '6LdK7xITAAzzAAJQTfL7fu6I-0aPl8KHHieAT_yJg';
+$_DVWA[ 'recaptcha_private_key' ] = '6LdK7xITAAzzAAJQTfL7fu6I-0aPl8KHHieAT_yJg';
 
 # Default security level
 #   Default value for the security level with each session.
